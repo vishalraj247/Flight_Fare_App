8fe326630ca9757490be79569f7d9efbe06c27f2
Vishal Raj <vishalraj247@gmail.com>
2023-11-09 23:13:56 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from src.data.data_preprocessor import DataPreprocessor
+from src.data.ml_model_data_preprocessor import PreProcessor
 import joblib
 import tensorflow as tf
 import pandas as pd
@@ -46,12 +47,13 @@ if st.button("Predict"):
         'preprocessor and mappings/avg_features_dl.csv'
     )
 
+    ml_preprocessor = PreProcessor()
+    prediction_ronik = ml_preprocessor.preprocess_for_user_input(user_input, 'preprocessor and mappings/mapped_average_values_ronik.csv')
+
     # Paths to all the students' models
     model_student_mapping = {
         "models/best_model-vishal_raj": "Vishal Raj's Model",
         "models/best_model_Shivatmak": "Shivatmak's Model",
-    #    "models/ronik_model": "Ronik's Model",
-    #    "models/student4_model": "Student 4's Model"
     }
 
     # Loop through each model, predict and display results
@@ -88,6 +90,5 @@ if st.button("Predict"):
         elif "Shivatmak" in model_path:
             predicted_fare1 = model.predict([startingAirport, destinationAirport, segmentsCabinCode, numerical_features])
             st.write(f"Prediction from {student_name}: ${predicted_fare1[0][0]:.2f}")
-#        elif "Ronik" in model_path:
-#            predicted_fare2 = 
-#            st.write(f"Prediction from {student_name}: ${predicted_fare1[0][0]:.2f}")
+
+    st.write(f"Prediction from Ronik's Model: ${prediction_ronik[0]:.2f}")
